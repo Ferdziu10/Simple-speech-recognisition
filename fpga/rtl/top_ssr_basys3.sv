fdeddba93d93e34bc9358702a27cce9b519daee2
ferdziu103 <ferdziu103@gmail.com>
2024-07-18 16:30:00 +0200

    dodany zegar ustawiony plik top fpga

--- fpga/rtl/top_ssr_basys3.sv
+++ fpga/rtl/top_ssr_basys3.sv
@@ -6,7 +6,7 @@
  * Modified by:
  * 2023  AGH University of Science and Technology
  * MTM UEC2
- * Piotr Kaczmarczyk
+ * Kacper Ferdek
  *
  * Description:
  * Top level synthesizable module including the project top and all the FPGA-referred modules.
@@ -14,15 +14,13 @@
 
 `timescale 1 ns / 1 ps
 
-module top_vga_basys3 (
+module top_ssr_basys3 (
     input  wire clk,
     input  wire btnC,
-    output wire Vsync,
-    output wire Hsync,
-    output wire [3:0] vgaRed,
-    output wire [3:0] vgaGreen,
-    output wire [3:0] vgaBlue,
-    output wire JA1
+    input  wire sw0,
+    output wire JA3,
+    output wire JA4,
+    output wire led0
 );
 
 
@@ -30,14 +28,14 @@ module top_vga_basys3 (
  * Local variables and signals
  */
 
-wire clk_in, clk_fb, clk_ss, clk_out;
+wire clk100MHz;
+wire clk40MHz;
 wire locked;
-wire pclk;
-wire pclk_mirror;
+
 
 (* KEEP = "TRUE" *)
 (* ASYNC_REG = "TRUE" *)
-logic [7:0] safe_start = 0;
+
 // For details on synthesis attributes used above, see AMD Xilinx UG 901:
 // https://docs.xilinx.com/r/en-US/ug901-vivado-synthesis/Synthesis-Attributes
 
@@ -46,85 +44,39 @@ logic [7:0] safe_start = 0;
  * Signals assignments
  */
 
-assign JA1 = pclk_mirror;
 
 
 /**
  * FPGA submodules placement
  */
 
-IBUF clk_ibuf (
-    .I(clk),
-    .O(clk_in)
-);
-
-MMCME2_BASE #(
-    .CLKIN1_PERIOD(10.000),
-    .CLKFBOUT_MULT_F(10.000),
-    .CLKOUT0_DIVIDE_F(25.000)
-) clk_in_mmcme2 (
-    .CLKIN1(clk_in),
-    .CLKOUT0(clk_out),
-    .CLKOUT0B(),
-    .CLKOUT1(),
-    .CLKOUT1B(),
-    .CLKOUT2(),
-    .CLKOUT2B(),
-    .CLKOUT3(),
-    .CLKOUT3B(),
-    .CLKOUT4(),
-    .CLKOUT5(),
-    .CLKOUT6(),
-    .CLKFBOUT(clk_fb),
-    .CLKFBOUTB(),
-    .CLKFBIN(clk_fb),
-    .LOCKED(locked),
-    .PWRDWN(1'b0),
-    .RST(1'b0)
-);
-
-BUFH clk_out_bufh (
-    .I(clk_out),
-    .O(clk_ss)
-);
-
-always_ff @(posedge clk_ss)
-    safe_start <= {safe_start[6:0],locked};
-
-BUFGCE #(
-    .SIM_DEVICE("7SERIES")
-) clk_out_bufgce (
-    .I(clk_out),
-    .CE(safe_start[7]),
-    .O(pclk)
-);
-
 // Mirror pclk on a pin for use by the testbench;
 // not functionally required for this design to work.
+(* CORE_GENERATION_INFO = "clk_wiz_0,clk_wiz_v6_0_9_0_0,{component_name=clk_wiz_0,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,enable_axi=0,feedback_source=FDBK_AUTO,PRIMITIVE=MMCM,num_out_clk=2,clkin1_period=10.000,clkin2_period=10.000,use_power_down=false,use_reset=false,use_locked=true,use_inclk_stopped=false,feedback_type=SINGLE,CLOCK_MGR_TYPE=NA,manual_override=false}" *)
 
-ODDR pclk_oddr (
-    .Q(pclk_mirror),
-    .C(pclk),
-    .CE(1'b1),
-    .D1(1'b1),
-    .D2(1'b0),
-    .R(1'b0),
-    .S(1'b0)
-);
-
+  clk_wiz_0_clk_wiz inst
+  (
+  // Clock out ports  
+  .clk100MHz(clk100MHz),
+  .clk40MHz(clk40MHz),
+  // Status and control signals               
+  .locked(locked),
+ // Clock in ports
+  .clk(clk)
+  );
 
 /**
  *  Project functional top module
  */
 
-top_vga u_top_vga (
-    .clk(pclk),
+top_ssr u_top_ssr (
+    .clk(clk100MHz),
     .rst(btnC),
-    .r(vgaRed),
-    .g(vgaGreen),
-    .b(vgaBlue),
-    .hs(Hsync),
-    .vs(Vsync)
+    .mic(sw0),
+    .scl(JA3),
+    .sda(JA4),
+    .out(led0)
+
 );
 
 endmodule
