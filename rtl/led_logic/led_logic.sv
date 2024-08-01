5e3deba886c70f2f7ce0cdce89aae6362f5fc4c7
H1tchie <mateuszgibas@vp.pl>
2024-08-01 19:22:07 +0200

    usuniento niepotrzebyn switch(uzyto go w logice magazynującej dane)

--- rtl/led_logic/led_logic.sv
+++ rtl/led_logic/led_logic.sv
@@ -2,7 +2,6 @@ module led_logic(
     input  logic clk,          // zegar systemowy
     input  logic rst,           // reset
     input  wire [1:0] speech_rec, // wynik rozpoznania mowy 0(nie rozponzano nic), 1(on), 2(off) 
-    input  logic sw0,           // przełącznik
     output logic led0           // dioda
 );
 
@@ -16,16 +15,13 @@ always_ff @(posedge clk) begin
         led0 <= led0_nxt;
 end
 
-always_comb begin
-    if(sw0) begin
-        case(speech_rec) 
-            2'b00: led0_nxt = led0;
-            2'b01: led0_nxt = 1;
-            2'b10: sled0_nxt = 0;
-            default: led0_nxt= led0;
-        endcase
-    end else
-        led0_nxt= led0
+always_comb begin    
+    case(speech_rec) 
+        2'b00: led0_nxt = led0;
+        2'b01: led0_nxt = 1;
+        2'b10: sled0_nxt = 0;
+        default: led0_nxt= led0;
+    endcase
 end
 
 endmodule
