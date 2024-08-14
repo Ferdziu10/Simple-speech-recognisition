eee881060513c9f3a581c30d9f81186958645193
H1tchie <mateuszgibas@vp.pl>
2024-08-14 13:19:57 +0200

    zmieniono rozmiar wyjsc i wejsc

--- rtl/audio_processing/axi_to_simple.sv
+++ rtl/audio_processing/axi_to_simple.sv
@@ -1,18 +1,18 @@
 module axi_to_simple (
     input logic clk,
     input logic reset,
-    input logic [31:0] axi_tdata,
+    input logic [15:0] axi_tdata,
     input logic axi_tvalid,
     output logic axi_tready,
     input logic axi_tlast,         // Dodany sygnał TLAST
-    output logic [31:0] simple_data_out,
+    output logic [15:0] simple_data_out,
     output logic simple_valid_out,
     output logic simple_last_out   // Sygnał TLAST na wyjściu prostego interfejsu
 );
 
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-        simple_data_out <= 32'b0;
+        simple_data_out <= 16'b0;
         simple_valid_out <= 1'b0;
         simple_last_out <= 1'b0;    // Zresetowanie sygnału TLAST
         axi_tready <= 1'b1;
--- rtl/audio_processing/simple_to_axi.sv
+++ rtl/audio_processing/simple_to_axi.sv
@@ -1,17 +1,17 @@
 module simple_to_axi (
     input logic clk,
     input logic reset,
-    input logic [31:0] simple_data_in,
+    input logic [15:0] simple_data_in,
     input logic simple_valid_in,
     output logic simple_ready_out,
-    output logic [31:0] axi_tdata,
+    output logic [15:0] axi_tdata,
     output logic axi_tvalid,
     input logic axi_tready
 );
 
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-        axi_tdata <= 32'b0;
+        axi_tdata <= 16'b0;
         axi_tvalid <= 1'b0;
         simple_ready_out <= 1'b1;
     end else begin
