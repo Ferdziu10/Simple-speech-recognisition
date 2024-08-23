e1f37b56a7f7e3ce58319bac446b09fadaec07dd
H1tchie <mateuszgibas@vp.pl>
2024-08-23 16:55:25 +0200

    dodano port valid do mean/std i jego opoznienie o jeden takt

--- rtl/audio_processing/mean_std.sv
+++ rtl/audio_processing/mean_std.sv
@@ -1,8 +1,10 @@
 module mean_std (
     input logic clk,
     input logic rst,
+    input logic valid_in,
     input logic [15:0] data_in [0:39],
     output logic [15:0] features [0:25]
+    output logic valid_out,
 );
 
     // Zmienne pomocnicze
@@ -14,20 +16,24 @@ module mean_std (
     logic [31:0] guess, guess_next;
     logic [31:0] difference;
     logic [15:0] i;
+    logic valid_nxt;
     logic converged;
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
+            valid_out   <= 0;
             features[0] <= 0;
             features[1] <= 0;
         end else begin
             // Zapisanie wyników na wyjściu
             features[0] <= mean_nxt[15:0];    // Zapisujemy obliczoną srednia
             features[1] <= stddev_nxt[15:0];  // Zapisujemy obliczone odchyleneis tandardwoe
+            valid_out <= valid_nxt;
         end
     end
 
     always_comb begin
+        valid_nxt = valid_in;
         sum_nxt = 0;
         sum_sq_nxt = 0;
 
