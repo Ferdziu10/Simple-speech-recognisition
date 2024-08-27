462f5f0ece55e8fb6d193068199d066881881af7
ferdziu103 <ferdziu103@gmail.com>
2024-08-27 23:43:34 +0200

    nn opt

--- python/model_weights.py
+++ python/model_weights.py
@@ -5,13 +5,13 @@ from tensorflow.keras.models import load_model
 model = load_model('sound_classification_model.h5')
 
 # Get weights of the first dense layer
-weights, biases = model.layers[6].get_weights()
+weights, biases = model.layers[4].get_weights()
 
 # Quantize weights and biases (example using 8-bit quantization)
-weights_q = np.round(weights * 32767).astype(np.int16)
-biases_q = np.round(biases * 32767).astype(np.int16)
+weights_q = np.round(weights * 127).astype(np.int8)
+biases_q = np.round(biases * 127).astype(np.int8)
 
 # Save weights and biases to a file
-np.savetxt('dense24_weights.txt', weights_q, fmt='%d')
-np.savetxt('dense24_biases.txt', biases_q, fmt='%d')
+np.savetxt('generated_files/dense_weights3.txt', weights_q, fmt='%d')
+np.savetxt('generated_files/dense_biases3.txt', biases_q, fmt='%d')
 
