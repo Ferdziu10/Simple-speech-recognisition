3b52ea2da1fc46347065c05f107679fecd7bd7bb
H1tchie <mateuszgibas@vp.pl>
2024-08-27 23:00:06 +0200

    dodano ledtest

--- rtl/top_ssr.sv
+++ rtl/top_ssr.sv
@@ -3,14 +3,24 @@ module top_ssr(
     input logic rst,
     inout logic scl,
     inout logic sda,
-
+    output logic led0
 );
+
+logic [11:0] data_wire;
 
 pmod_adc_ad7991 u_pmod_adc_ad7991(
     .clk,
     .rst,
     .sda,
     .scl,
+    .adc_ch0_data(data_wire)
+);
+
+ledtest u_ledtest(
+    .clk,
+    .rst,
+    .adc_data(data_wire),
+    .led0
 );
 
 
